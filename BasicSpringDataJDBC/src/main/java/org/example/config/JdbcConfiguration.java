8ae53bd3e1734cbb2c95c3a4b0cb1a37faec4217
Tona-Mangobyte <tona@mango-byte.com>
2023-04-02 10:07:21 +0700

    fixed

--- BasicSpringDataJDBC/src/main/java/org/example/config/JdbcConfiguration.java
+++ BasicSpringDataJDBC/src/main/java/org/example/config/JdbcConfiguration.java
@@ -1,7 +1,10 @@
 package org.example.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
@@ -14,7 +17,11 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableJdbcRepositories("org.example.repository")
+@PropertySource("classpath:application.properties")
 public class JdbcConfiguration extends AbstractJdbcConfiguration {
+    @Autowired
+    private Environment env;
+
     @Bean
     NamedParameterJdbcOperations operations() {
         return new NamedParameterJdbcTemplate(dataSource());
@@ -31,9 +38,9 @@ public class JdbcConfiguration extends AbstractJdbcConfiguration {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/simple_spring?serverTimezone=UTC");
-        dataSource.setUsername("root");
-        dataSource.setPassword("123456");
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.user"));
+        dataSource.setPassword(env.getProperty("db.pass"));
         return dataSource;
     }
 
--- BasicSpringJDBC/src/main/java/org/example/config/AppConfig.java
+++ BasicSpringJDBC/src/main/java/org/example/config/AppConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -11,14 +13,18 @@ import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "org.example")
+@PropertySource("classpath:application.properties")
 public class AppConfig {
+    @Autowired
+    private Environment env;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/simple_spring?serverTimezone=UTC");
-        dataSource.setUsername("root");
-        dataSource.setPassword("123456");
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.user"));
+        dataSource.setPassword(env.getProperty("db.pass"));
         return dataSource;
     }
 
